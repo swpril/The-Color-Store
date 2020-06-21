a7ec05231f338d394bf56dedb19eb2f2ebd9f621
Swpril Ahuja <ahuja.s0007@gmail.com>
2020-06-21 20:38:44 +0530

    styled colorBox

--- src/Components/ColorBox.js
+++ src/Components/ColorBox.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import '../Styles/ColorBox.css';
 
-const ColorBox = (props) => {
+const ColorBox = ({ background, name }) => {
     return (
-        <div className='colorBox' style={{ backgroundColor: props.background }}>
-            <span>{props.name}</span>
+        <div className='colorBox' style={{ background }}>
+            <div className='copy-container'>
+                <div className='box-content'>
+                    <span>{name}</span>
+                </div>
+                <button className='copy-button'>Copy </button>
+            </div>
+            <span className='more'>More</span>
         </div>
     )
 };
