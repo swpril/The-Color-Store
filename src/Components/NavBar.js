04b8a7286f7e7b42416b0ce1e0f0e0070489ce4c
Swpril Ahuja <ahuja.s0007@gmail.com>
2020-06-23 08:45:53 +0530

    added Select and Material-UI

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Slider from 'rc-slider';
+import { Select, MenuItem } from '@material-ui/core';
 import 'rc-slider/assets/index.css'
 import '../Styles/Navbar.css';
 
-const NavBar = ({ level, handleChange }) => {
+const NavBar = ({ level, handleChange, handleSelect }) => {
+    const [format, setFormat] = useState('hex');
+    const handleFormat = (e) => {
+        setFormat(e.target.value)
+        handleSelect(e.target.value);
+    }
     return (
         <header className='Navbar'>
             <div className='logo'>
@@ -14,6 +20,16 @@ const NavBar = ({ level, handleChange }) => {
                 <div className='slider'>
                     <Slider defaultValue={level} min={100} max={900} onAfterChange={handleChange} step={100} />
                 </div>
+            </div>
+            <div className='select-container'>
+                <Select value={format} onChange={handleFormat}>
+                    <MenuItem value='hex'> HEX- #FFFFFF
+                    </MenuItem>
+                    <MenuItem value='rgb'> RGB- rgb(r,g,b)
+                    </MenuItem>
+                    <MenuItem value='rgba'> RGBA- rgba(r,g,b,a)
+                    </MenuItem>
+                </Select>
             </div>
         </header>
     )
--- src/Components/Palette.js
+++ src/Components/Palette.js
@@ -5,15 +5,19 @@ import NavBar from './NavBar';
 
 const Palette = (props) => {
     const [level, setLevel] = useState(500);
+    const [format, setFormat] = useState('hexValue');
     const handleChange = (level) => {
         setLevel(level);
     }
+    const handleSelect = (e) => {
+        setFormat(e);
+    }
     const colorBox = props.palette.colors[level].map((color) => (
-        <ColorBox background={color.hexValue} key={color.name} name={color.name} />
+        <ColorBox background={color[format]} key={color.name} name={color.name} />
     ))
     return (
         <div className='palette'>
-            <NavBar level={level} handleChange={handleChange} />
+            <NavBar level={level} handleChange={handleChange} handleSelect={handleSelect} />
             <div className='palette-colors '>
                 {colorBox}
             </div>
