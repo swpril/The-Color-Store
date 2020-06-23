fc2f22858e7ae01dbabb0a639e08385eb578288d
Swpril Ahuja <ahuja.s0007@gmail.com>
2020-06-23 10:04:42 +0530

    added Snackbar and styled Select

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import Slider from 'rc-slider';
-import { Select, MenuItem } from '@material-ui/core';
+import { Select, MenuItem, Snackbar, IconButton } from '@material-ui/core';
+import { Close as CloseIcon } from '@material-ui/icons';
 import 'rc-slider/assets/index.css'
 import '../Styles/Navbar.css';
 
 const NavBar = ({ level, handleChange, handleSelect }) => {
     const [format, setFormat] = useState('hex');
+    const [open, setOpen] = useState(false)
     const handleFormat = (e) => {
         setFormat(e.target.value)
         handleSelect(e.target.value);
+        setOpen(true);
+    }
+    const handleOpen = () => {
+        setOpen(false);
     }
     return (
         <header className='Navbar'>
@@ -31,6 +37,19 @@ const NavBar = ({ level, handleChange, handleSelect }) => {
                     </MenuItem>
                 </Select>
             </div>
+            <Snackbar
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+                open={open}
+                autoHideDuration={2000}
+                message={<span id='message-id'>Format Changed to {format.toUpperCase()}</span>}
+                ContentProps={{
+                    "aria-describedby": "message-id"
+                }}
+                onClose={handleOpen}
+                action={[<IconButton onClick={handleOpen} color='inherit' key='close' aria-label='close'>
+                    <CloseIcon />
+                </IconButton>]}
+            />
         </header>
     )
 }
