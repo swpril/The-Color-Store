969c50dfabaa0439ad52103ace045d02357ae373
Swpril Ahuja <ahuja.s0007@gmail.com>
2020-06-26 20:21:30 +0530

    styled DraggableColorBox

--- src/Components/DraggableColorBox.js
+++ src/Components/DraggableColorBox.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Delete as DeleteIcon } from '@material-ui/icons';
 import useStyles from '../Styles/DraggableColorBox';
+import { IconButton } from '@material-ui/core';
 
 const DraggableColorBox = (props) => {
     const classes = useStyles(props);
     return (
         <div className={classes.root}>
-            {props.name}
+            <div className={classes.boxContent}>
+                <span>{props.name}</span>
+                <DeleteIcon className={classes.deleteIcon} />
+            </div>
         </div>
     )
 };
--- src/Styles/DraggableColorBox.js
+++ src/Styles/DraggableColorBox.js
@@ -8,7 +8,28 @@ const useStyles = makeStyles((theme) => ({
         display: 'inline-block',
         position: 'relative',
         marginBottom: '-4px',
-        backgroundColor: props => props.color
+        backgroundColor: props => props.color,
+        '&:hover svg': {
+            color: 'white',
+            transform: 'scale(1.2)'
+        }
+    },
+    boxContent: {
+        position: 'absolute',
+        padding: '10',
+        width: '100%',
+        left: 0,
+        color: 'rgba(0,0,0,0.5)',
+        bottom: 0,
+        fontSize: '12px',
+        textTransform: 'uppercase',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    deleteIcon: {
+        color: 'black',
+        transition: 'all 0.2s ease-in-out'
     }
 }))
 
