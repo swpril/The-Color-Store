b5fb5de628590bc5c18c8fa42650193a579fefb7
Swpril Ahuja <ahuja.s0007@gmail.com>
2020-06-26 12:26:53 +0530

    Added NewPaletteForm

--- src/Components/NewPaletteForm.js
+++ src/Components/NewPaletteForm.js
@@ -1,11 +1,89 @@
 import React from 'react';
+import clsx from 'clsx';
+import {
+    Drawer,
+    CssBaseline, AppBar, Toolbar,
+    Typography, Divider, IconButton
+} from '@material-ui/core';
+import {
+    Add as AddIcon, ChevronLeft as ChevronLeftIcon
+} from '@material-ui/icons';
+import useStyles from '../Styles/NewPaletteForm';
 
-const NewPaletteForm = () => {
+const NewPaletteFrom = () => {
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
     return (
-        <div>
-            hi
+        <div className={classes.root}>
+            <CssBaseline />
+            <AppBar
+                position="fixed"
+                className={clsx(classes.appBar, {
+                    [classes.appBarShift]: open,
+                })}
+            >
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        className={clsx(classes.menuButton, open && classes.hide)}
+                    >
+                        <AddIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap>
+                        The Color Store
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                className={classes.drawer}
+                variant="persistent"
+                anchor="left"
+                open={open}
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+            >
+                <div className={classes.drawerHeader}>
+                    <IconButton onClick={handleDrawerClose}>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                </div>
+                <Divider />
+                <Divider />
+            </Drawer>
+            <main
+                className={clsx(classes.content, {
+                    [classes.contentShift]: open,
+                })}
+            >
+                <div className={classes.drawerHeader} />
+                <Typography paragraph>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                    ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+                    facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+                    gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+                    donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+                    Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+                    imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+                    arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+                    donec massa sapien faucibus et molestie ac.
+        </Typography>
+            </main>
         </div>
-    )
-};
+    );
+}
 
-export default NewPaletteForm;
+export default NewPaletteFrom;
